8d120d0193f41ecf15ac06a5fc1af5811397434c
Philip Oseikhuemen Inegbedion <philliposeikhuemen@gmail.com>
2025-10-16 14:25:05 +0100

    feat(products): add ProductViewSet with search, filters, ordering and admin permissions

--- ecommerce-api/products/views.py
+++ ecommerce-api/products/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return bool(request.user and request.user.is_authenticated and request.user.is_staff)
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.select_related('category','created_by').all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = {
+        'category__name': ['exact'],
+        'price': ['gte', 'lte'],
+        'stock': ['gte', 'lte'],
+    }
+    search_fields = ['name', 'description', 'category__name']
+    ordering_fields = ['price', 'created_at', 'stock']
+
+    def perform_create(self, serializer):
+        # set creator if user is authenticated
+        user = None
+        if self.request.user and self.request.user.is_authenticated:
+            user = self.request.user
+        serializer.save(created_by=user)
