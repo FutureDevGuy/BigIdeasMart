7c785c89259c622db32c5349ecf588ab80775320
Philip Oseikhuemen Inegbedion <philliposeikhuemen@gmail.com>
2025-10-16 14:22:33 +0100

    chore(admin): register Category and Product in admin

--- ecommerce-api/products/admin.py
+++ ecommerce-api/products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug', 'created_at')
+    prepopulated_fields = {"slug": ("name",)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category', 'stock', 'created_at')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
